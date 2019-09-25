e4e70b43c5d2af883796c41db6874e0b94ceaab7
sai <saikirankondapalli03@gmail.com>
2019-09-25 18:49:51 -0400

    configured mongodb credentials to cloud

--- code/travelapp-boot/src/main/java/com/travellerapp/domain/Traveller.java
+++ code/travelapp-boot/src/main/java/com/travellerapp/domain/Traveller.java
@@ -4,6 +4,7 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection="Traveller")
 public class Traveller {
 
     public Traveller() {
--- code/travelapp-boot/src/main/java/com/travellerapp/repositories/TravellerRepository.java
+++ code/travelapp-boot/src/main/java/com/travellerapp/repositories/TravellerRepository.java
@@ -1,11 +1,11 @@
 package com.travellerapp.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.travellerapp.domain.Traveller;
 
 @Repository
-public interface TravellerRepository extends CrudRepository<Traveller, String> {
+public interface TravellerRepository extends MongoRepository<Traveller, String> {
 	
 }
