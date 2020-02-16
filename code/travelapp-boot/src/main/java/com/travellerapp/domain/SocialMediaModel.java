450df0a1c489d790af2bad2963f40b74836faadd
sai <saikirankondapalli03@gmail.com>
2020-02-16 17:42:15 -0500

    User API modifications

---
+++ code/travelapp-boot/src/main/java/com/travellerapp/domain/SocialMediaModel.java
@@ -0,0 +1,22 @@
+package com.travellerapp.domain;
+
+public class SocialMediaModel {
+	private String linkUrl;
+	private String linkDescription;
+
+	public String getLinkUrl() {
+		return linkUrl;
+	}
+
+	public void setLinkUrl(String linkUrl) {
+		this.linkUrl = linkUrl;
+	}
+
+	public String getLinkDescription() {
+		return linkDescription;
+	}
+
+	public void setLinkDescription(String linkDescription) {
+		this.linkDescription = linkDescription;
+	}
+}
